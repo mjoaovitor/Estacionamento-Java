d190462134404c3a06d4d145423a9a6250715341
mjoaovitor <joao_victor-1999@hotmail.com>
2022-01-28 17:03:29 -0300

    Finalização do Aprendizado de Java DIO

--- Projeto Estacionamento/src/com/joaos/classes/Main.java
+++ Projeto Estacionamento/src/com/joaos/classes/Main.java
@@ -4,15 +4,39 @@ package com.joaos.classes;
 import com.joaos.classes.veiculos.TipoCarro;
 import com.joaos.classes.veiculos.Veiculo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main (String [] args) {
 //        System.out.println("Hello World"); // Teste 1
 //        System.out.println("Meu nome é Joao Vitor"); // Teste 2
 
-        Veiculo veiculo = new Veiculo( placa: "ABC-1010");
+        List<Veiculo> veiculos = new ArrayList<>();
+        Veiculo veiculo = new Veiculo();
+        Veiculo veiculo2 = new Veiculo();
+        Veiculo veiculo3 = new Veiculo();
+
         veiculo.setTipoCarro(TipoCarro.MOTO);
-        System.out.println(veiculo.getplaca());
+        veiculo.setPlaca("213321");
+
+        veiculo2.setTipoCarro(TipoCarro.CARRO);
+        veiculo2.setPlaca("kkkkkkk");
+
+        veiculo3.setTipoCarro(TipoCarro.UTILITARIO);
+        veiculo3.setPlaca("1asdasda");
+
+        veiculos.add(veiculo);
+        veiculos.add(veiculo2);
+        veiculos.add(veiculo3);
+
+        System.out.println("Qual o tamanho da Lista?? "+ veiculos.size());
+        veiculos.forEach(v->System.out.println(veiculo.getPlaca()+" "+veiculo.getTipoCarro()));
+
+
+
+
 
 
     }
--- Projeto Estacionamento/src/com/joaos/classes/veiculos/Veiculo.java
+++ Projeto Estacionamento/src/com/joaos/classes/veiculos/Veiculo.java
@@ -16,6 +16,70 @@ public class Veiculo {
     public Veiculo(String Placa) {
         this.placa = Placa;
     }
+
+    public  Veiculo(){
+
+    }
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public double getFatorEstacionamento() {
+        return fatorEstacionamento;
+    }
+
+    public void setFatorEstacionamento(double fatorEstacionamento) {
+        this.fatorEstacionamento = fatorEstacionamento;
+    }
+
+    public String getHoraEntrada() {
+        return horaEntrada;
+    }
+
+    public void setHoraEntrada(String horaEntrada) {
+        this.horaEntrada = horaEntrada;
+    }
+
+    public String getHoraSaida() {
+        return horaSaida;
+    }
+
+    public void setHoraSaida(String horaSaida) {
+        this.horaSaida = horaSaida;
+    }
+
+    public double getTotalEstacionamento() {
+        return totalEstacionamento;
+    }
+
+    public void setTotalEstacionamento(double totalEstacionamento) {
+        this.totalEstacionamento = totalEstacionamento;
+    }
+
+    public TipoCarro getTipoCarro() {
+        return tipoCarro;
+    }
+
     public void setTipoCarro(TipoCarro tipoCarro) {
         this.tipoCarro = tipoCarro;
     }
